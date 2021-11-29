a8dcb2646008cb1797041c1ed43ccb3ab6d257bb
wavelolz <71971775+wavelolz@users.noreply.github.com>
2021-11-29 10:35:35 +0800

    Update freezshow.py

--- freezshow.py
+++ freezshow.py
@@ -1,16 +1,25 @@
-
-
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-data = np.loadtxt("D:/freezing of gait/dataset_fog_release/dataset/S01R01.txt").transpose(1, 0)
-interval = 10
-move = 3
-start = 1700
-stay_time = 0.7
-output_data = [2, 4, 6, 7, 8, 9]
 
+#-----參數宣告區
+path = "D:/freezing of gait/dataset_fog_release/dataset/S01R01.txt"
+interval = 10 # 每個視窗要包含的秒數
+move = 3 # 每次移動要前進的秒數
+start = 1000 # 起始的秒數
+stay_time = 0.7 # 每個視窗停留的秒數
+output_data = [2, 3, 4, 5, 6, 7, 8, 9] # 想要觀看的data的column index
 
+# 這是一個將原始raw data加速度以移動視窗來呈現的程式
+# 希望藉由資料視覺化能夠找出在raw data中有一些可以不需要
+# 丟進model裡的trash data
+# 在視窗中如果為黃底視窗，其label為0
+# 白底視窗，其label為1
+# 紅底視窗其label為2
+ 
+#-----
+
+data = np.loadtxt(path).transpose(1, 0)
 index = np.array([int(start * 64), int(start * 64 + interval * 64)])
 x = data[0][index[0] : index[1]] / 1000
 y = []
@@ -26,6 +35,7 @@ def plotting(i):
     ax[i].fill_between(x, y1 = min(min(y[i]), -2), y2 = max(max(y[i]), 2), where = (label == 0), alpha = 0.3, color = "yellow")
     ax[i].fill_between(x, y1 = min(min(y[i]), -2), y2 = max(max(y[i]), 2), where = (label == 2), alpha = 0.3, color = "red")
     ax[i].set_ylim([min(min(y[i]), -2), max(max(y[i]), 2)])
+    ax[i].grid(True)
 
 for i in range(len(output_data) - 1):
     ax[i].tick_params(
