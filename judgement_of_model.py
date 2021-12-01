8b3af25ffb46fafcf84a9498204abaa463603b02
wavelolz <71971775+wavelolz@users.noreply.github.com>
2021-12-01 11:12:01 +0800

    Update judgement_of_model.py

--- judgement_of_model.py
+++ judgement_of_model.py
@@ -2,42 +2,23 @@ import pandas as pd
 import numpy as np
 
 ##--------
-# 這是一個計算模型準確度的程式，輸入依序為
-# 1. (actual is nonevent and predict is nonevent)
-# 2. (actual is nonevent but predict is event)
-# 3. (actual is event but predict is nonevent)
-# 4. (actual is event and predict is event)
 
-#      0    1
-#   0  67   45
-#
-#   1  21   76
-# 範例數入: 67 45 21 76
 
 
 ##--------
-def judge(a1, a2, a3, a4):
-  tn, fp, fn, tp = a1, a2, a3, a4
-  sen = round(tp / (tp + fn), 4)
-  spec = round(tn / (fp + tn), 4)
-  accu = round((tp + tn) / sum([tp, tn, fn, fp]), 4)
-  pre = round(tp / (tp + fp), 4)
-  f1 = round((2 * sen * pre) / (sen + pre), 4)
-  arr = pd.DataFrame(np.array([[tn, fp],
-                              [fn, tp]]))
+def judge(df):
 
-  arr.columns = ["Predict_is_nonevent", "Predict_is_event"]
-  arr.index = ["Real_is_nonevent", "Real_is_event"]
+  result = []
+  for i in range(len(df)):
+    tn, fp, fn, tp = df[i, 0], df[i, 1], df[i, 2], df[i, 3]
+    sen = round(tp / (tp + fn), 4)
+    spec = round(tn / (fp + tn), 4)
+    accu = round((tp + tn) / sum([tp, tn, fn, fp]), 4)
+    pre = round(tp / (tp + fp), 4)
+    f1 = round((2 * sen * pre) / (sen + pre), 4)
+    result.append([sen, spec, accu, pre, f1])
+  result = pd.DataFrame(result)
 
-  result = {
-      "sensitivity" : sen,
-      "specificity" : spec,
-      "accuracy" : accu,
-      "precision" : pre,
-      "F1-score" : f1
-  }
-  print("---------------------")
+  result.columns = ["Sensitivity", "Specificity", "Accuracy", "Precision", "F1-score"]
+  result.index = ["Model_" + str((i + 1)) for i in range(len(df))]
   print(result)
-  print("---------------------")
-  print("Confusion matrix:")
-  print(arr)
