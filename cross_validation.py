863eda60af9ba7a2faaeaf6fadc4011887b1515e
wavelolz <71971775+wavelolz@users.noreply.github.com>
2021-12-20 15:56:11 +0800

    Update cross_validation.py

--- cross_validation.py
+++ cross_validation.py
@@ -1,9 +1,3 @@
-
-import pandas as pd
-import numpy as np
-from sklearn import tree
-from sklearn.metrics import confusion_matrix
-
 data = pd.read_csv("D:/data/balanced_cut_all_1sec0.75olp.csv")
 data = data[['y_u_max', 'z_u_max', 'x_u_max', 'y_a_fft_totalpower', 'y_u_mean',
        'y_a_std', 'y_a_fft_min', 'y_u_std', 'y_a_fft_max',
@@ -11,26 +5,36 @@ data = data[['y_u_max', 'z_u_max', 'x_u_max', 'y_a_fft_totalpower', 'y_u_mean',
        'z_u_fft_totalpower', 'y_u_med', 'u_egv1', 'z_u_fft_min',
        'y_t_fft_totalpower', 'u_egv2', 'y_u_min', 'z_t_min', 'y_t_std',
        'z_a_fft_totalpower', 'y_u_fft_min', "label", "id"]]
-
+print("資料裡是否有patient4跟patient10的資料[y/n]??")
+I = input()
+if I == "y":
+    rounding_index = np.arange(1, 11, 1)
+elif I == "n":
+    rounding_index = [1, 2, 3, 5, 6, 7, 8, 9]
 
 
 result = []
-for i in [1, 2, 3, 5, 6, 7, 8, 9]:
+
+for i in rounding_index:
     # data selection
     non_freeze = [i, 4, 10]
     data_training = data.drop(data.loc[data["id"].isin(non_freeze)].index)
     data_testing = data.loc[data["id"] == i]
 
-    # train model
-    dt = tree.DecisionTreeClassifier()
-    dt.fit(data_training.iloc[:, :-2], data_training.iloc[:, -2])
-    
-    # predict
-    predict = dt.predict(data_testing.iloc[:, :-2])
+# 僅需修改這部分
+#--------------------------------------------------------------------
+    # train model                                                    #
+    dt = tree.DecisionTreeClassifier()                               #
+    dt.fit(data_training.iloc[:, :-2], data_training.iloc[:, -2])    #
+                                                                     # 
+    # predict                                                        #
+    predict = dt.predict(data_testing.iloc[:, :-2])                  #
+                                                                     #
+#--------------------------------------------------------------------
     result.append(list(confusion_matrix(data_testing.iloc[:, -2], predict).ravel()))
 
 
-row_name = [f"leave-P{i}-out" for i in [1, 2, 3, 5, 6, 7, 8, 9]]
+row_name = [f"leave-P{i}-out" for i in rounding_index]
 df = pd.DataFrame(result)
 df.index = row_name
 
